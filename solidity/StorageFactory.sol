bee66cc6f0d61bd0663d2c2b24074af9eeb0d2a6
Yousef Okasha <yousefokasha61@gmail.com>
2022-05-28 16:56:21 +0200

    first commit

---
+++ solidity/StorageFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import './SimpleStorage.sol';
+
+contract StorageFactory is SimpleStorage {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorage() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    }
+}
